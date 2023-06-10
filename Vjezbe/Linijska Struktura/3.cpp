b8fb925a6f86ad5db2c9ffee16b0952ccf7985aa
Danin Sadzak <daninsadzak@gmail.com>
2023-06-10 22:53:00 +0200

    Editovan settings.json

--- Vjezbe/Linijska Struktura/3.cpp
+++ Vjezbe/Linijska Struktura/3.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 int main() {
+    int a, b;
+    cout << "Unesite stranice a i b!\n";
+    cin >> a >> b;
 
+    cout << "Povrsina pravougaonika: " << a * b << '\n';
+    cout << "Obim pravougaonika: " << 2 * (a + b);
     return 0;
 }
 /*
