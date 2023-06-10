5db2f037af164748d385b612efc18f908d8e7d50
Danin Sadzak <daninsadzak@gmail.com>
2023-06-10 22:48:02 +0200

    Vjezbe - Linijska Struktura - Zavrsen 2. zadatak

--- Vjezbe/Linijska Struktura/2.cpp
+++ Vjezbe/Linijska Struktura/2.cpp
@@ -2,9 +2,17 @@
 using namespace std;
 
 int main() {
+    int a, b;
+    cout << "Unesite brojeve a i b!\n";
+    cin >> a >> b;
 
+    cout << "Njihov zbir: " << a + b << '\n';
+    cout << "Njihova razlika: " << a - b << '\n';
+    cout << "Njihov proizvod: " << a * b << '\n';
+    cout << "Dvostruka vrijednost prvog broja: " << a * a;
     return 0;
 }
 /*
-Napisati program koji za dva unesena cijela broja racuna i ispisuje njihov zbir. 
+Napisati program koji za dva unesena cijela broja racuna i ispisuje njihov zbir, razliku, proizvod i dvostruku vrijednost prvog
+broja. 
 */
