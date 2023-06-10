2525e78ab0a1aa096925d66b867b13e1a66454b3
Danin Sadzak <daninsadzak@gmail.com>
2023-06-10 22:43:01 +0200

    Vjezbe - Linijska Struktura - Zavrsen 1. zadatak

--- Vjezbe/Linijska Struktura/1.cpp
+++ Vjezbe/Linijska Struktura/1.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main() {
-
+    int a, b;
+    cout << "Unesite brojeve a i b!\n";
+    cin >> a >> b;
+    
+    cout << "Njihov zbir: " << a + b;
     return 0;
 }
 /*
