2298b7d6d5ef8a62e49f1f8a77b66f838c053a65
Danin Sadzak <daninsadzak@gmail.com>
2023-06-13 23:16:26 +0200

    Vjezbe - Linijska Struktura - Zavrsen 9. zadatak

--- Vjezbe/Linijska Struktura/9.cpp
+++ Vjezbe/Linijska Struktura/9.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main() {
-
+    int br;
+    cout << "Unesite trocifreni broj!\n";
+    cin >> br;
+    cout << "Suma cifara ucitanog trocifrenog broja: " << ((br / 100) + ((br/10) % 10) + (br % 10));
     return 0;
 }
 /*
