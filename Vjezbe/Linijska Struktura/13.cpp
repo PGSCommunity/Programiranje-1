c0ad35bc598ba7a1245b0118faa0bd50d69f4541
Danin Sadzak <daninsadzak@gmail.com>
2023-06-13 23:38:56 +0200

    Vjezbe - Linijska Struktura - Zavrsen 13. zadatak

--- Vjezbe/Linijska Struktura/13.cpp
+++ Vjezbe/Linijska Struktura/13.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-
+    float d;
+    cout << "Unestie dijagonalu d!\n";
+    cin >> d;
+    float a = (d * sqrt(2)) / 2;
+    cout << "Povrsina kvadrata: " << a * a << '\n';
+    cout << "Obim kvadrata: " << 4 * a; 
     return 0;
 }
 /*
