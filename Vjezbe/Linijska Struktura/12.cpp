2ab9b33582ca11bbc1febeaa3e1859e43c18fb4a
Danin Sadzak <daninsadzak@gmail.com>
2023-06-13 23:31:34 +0200

    Vjezbe - Linijska Struktura - Postavljen 12. zadatak

---
+++ Vjezbe/Linijska Struktura/12.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji za tijelo mase (m) oada visine (h) metara brzinom V. Kolika je ukupna energija tijela?
+*/
