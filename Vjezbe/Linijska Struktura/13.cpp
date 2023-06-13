c49b306d1da97462d2eda22be7e044f3289c9bd4
Danin Sadzak <daninsadzak@gmail.com>
2023-06-13 23:36:28 +0200

    Vjezbe - Linijska Struktura - Postavljen 13. zadatak

---
+++ Vjezbe/Linijska Struktura/13.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji za ucitanu dijagonalu kvadrata racuna i ispisuje njegovu povrsinu i obim
+*/
