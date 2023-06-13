1065a839bf3745722b6ab840217545b1f061d074
Danin Sadzak <daninsadzak@gmail.com>
2023-06-13 23:21:23 +0200

    Vjezbe - Linijska Struktura - Zavrsen 10. zadatak

--- Vjezbe/Linijska Struktura/10.cpp
+++ Vjezbe/Linijska Struktura/10.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main() {
-
+    int br;
+    cout << "Unesite cetverocifreni broj!\n";
+    cin >> br;
+    cout << "Reciprocna vrijednost proizvoda cifara unesenog cetverocifrenog broja: " << (float)(1 / ((br / 1000) * ((br / 100) % 10) * ((br / 10) % 10) * (br % 10))); 
     return 0;
 }
 /*
