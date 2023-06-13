27150539001ae61224d859079765c264471fca6a
Danin Sadzak <daninsadzak@gmail.com>
2023-06-13 23:41:57 +0200

    Vjezbe - Linijska Struktura - Postavljen 14. zadatak

---
+++ Vjezbe/Linijska Struktura/14.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji za dva ucitana trocifrena broja racuna i ispisuje sumu cifara stotica, proizvod desetica i razliku jedinica
+*/
