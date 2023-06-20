9b858577146583f6089f4bc742938848fd7fd83e
Danin Sadzak <daninsadzak@gmail.com>
2023-06-20 17:41:15 +0200

    Vjezbe - Linijska Struktura - Zavrsen 18. zadatak

--- Vjezbe/Linijska Struktura/18.cpp
+++ Vjezbe/Linijska Struktura/18.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 int main() {
-
+    int sati, minute;
+    cout << "Unesite sate i minute!\n";
+    cin >> sati >> minute;
+    cout << "Vrijednost u sekundama: " << (sati * 3600) + (minute * 60); 
     return 0;
 }
 /*
-Napisati program koji za ucitane sate, minute, i sekunde ispisuje vrijeme u sekundama
+Napisati program koji za ucitane sate i minute ispisuje vrijeme u sekundama
 */
