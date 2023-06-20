0b94357981f69e7eeece10bc0b9a185bc0efc89b
Danin Sadzak <daninsadzak@gmail.com>
2023-06-20 17:56:23 +0200

    Pripreme - Linijska Struktura - Zavrsen 4. zadatak

--- Pripreme/Linijska Struktura/4.cpp
+++ Pripreme/Linijska Struktura/4.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 int main() {
-
+    int trocifreni1, trocifreni2;
+    cout << "Unesite dva trocifrena broja!\n";
+    cin >> trocifreni1 >> trocifreni2;
+    int d1 = (trocifreni1 / 10) % 10;
+    int d2 = (trocifreni2 / 10) % 10;
+    cout << "Razlika desetica: " << d1 - d2;
     return 0;
 }
 /*
