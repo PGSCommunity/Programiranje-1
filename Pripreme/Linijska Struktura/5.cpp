de4b4409477a0a5b890ca8ed7f5a613aaeabc763
Danin Sadzak <daninsadzak@gmail.com>
2023-06-20 17:58:26 +0200

    Pripreme - Linijska Struktura - Postavljen 5. zadatak

---
+++ Pripreme/Linijska Struktura/5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Radius zemlje je 6370km, za koje vrijeme ce avion obletjeti zemlju ako leti na visi od n kilometara, brzinom V
+*/
