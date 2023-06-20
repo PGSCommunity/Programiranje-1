a7380110a81aaf56768a9943006401c65a1b6097
Danin Sadzak <daninsadzak@gmail.com>
2023-06-20 17:52:58 +0200

    Pripreme - Linijska Struktura - Zavrsen 3. zadatak

--- Pripreme/Linijska Struktura/3.cpp
+++ Pripreme/Linijska Struktura/3.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 int main() {
-
+    int dvocifreni;
+    cout << "Unesite dvocifreni broj!\n";
+    cin >> dvocifreni;
+    int d = dvocifreni / 10;
+    int j = dvocifreni % 10;
+    cout << "Proizvod cifara " << d + j << '\n';
+    cout << "Kolicnik cifara " << d / j;
     return 0;
 }
 /*
