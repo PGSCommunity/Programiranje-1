bd7d658d98138c4ea6832980f2352fd11efa36a4
Danin Sadzak <daninsadzak@gmail.com>
2023-06-20 17:51:12 +0200

    Pripreme - Linijska Struktura - Postavljen 3. zadatak

---
+++ Pripreme/Linijska Struktura/3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji ucitava dvocifreni broj i ispisuje proizvod cifara i kolicnik prve i druge cifre
+*/
