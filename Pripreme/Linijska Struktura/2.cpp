1e834a32a85c4423a353fcab59299a068ea7864d
Danin Sadzak <daninsadzak@gmail.com>
2023-06-20 17:49:42 +0200

    Pripreme - Linijska Struktura - Zavrsen 2. zadatak

--- Pripreme/Linijska Struktura/2.cpp
+++ Pripreme/Linijska Struktura/2.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 int main() {
-
+    float kelvin;
+    const float celsius = -273.15;
+    cout << "Unesite vrijednost u kelivinima!\n";
+    cin >> kelvin;
+    cout << "Vrijednost u celzijusima: " << kelvin + celsius;
     return 0;
 }
 /*
-Napisati program za pretvaranje Kelvina u Celzijuse (1K = 272,15C)
+Napisati program za pretvaranje Kelvina u Celzijuse (1K = -272,15C)
 */
