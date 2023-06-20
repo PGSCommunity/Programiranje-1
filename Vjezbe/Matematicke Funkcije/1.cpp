874acbf5a65e8c6ca8bcef421af626f7d3809da2
Danin Sadzak <daninsadzak@gmail.com>
2023-06-20 19:14:52 +0200

    Vjezbe - Matematicke Funkcije - Postavljen 1. zadatak

---
+++ Vjezbe/Matematicke Funkcije/1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Treba unijeti koordinate tačaka A i B, pa izračunati njihovu udaljenost u koordinatnom sistemu. 
+*/
