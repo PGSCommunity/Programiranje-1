f2d6ef87c86fb022a968dac43959d15ec52f65da
Danin Sadzak <daninsadzak@gmail.com>
2023-06-20 19:19:59 +0200

    Vjezbe - Matematicke Funkcije - Postavljen 2. zadatak

---
+++ Vjezbe/Matematicke Funkcije/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+U pravouglom trouglu poznat je ugao alfa (izražen u stepenima) i kateta a (u cm). Treba izračunati vrijednost hipotenuze c. 
+*/
