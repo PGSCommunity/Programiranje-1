1db063e7ac0062211aea92dd2c0930a45d9a178e
Danin Sadzak <daninsadzak@gmail.com>
2023-06-20 18:57:47 +0200

    Pripreme - Linijska Struktura - Postavljen 7. zadatak

---
+++ Pripreme/Linijska Struktura/7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program za izracun visine trougla ako je poznata povrsina i stranica trougla (P = a * h / 2)
+*/
