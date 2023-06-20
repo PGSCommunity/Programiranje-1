6ab9b2781f5d2703f18222538a42a230a14a4ab0
Danin Sadzak <daninsadzak@gmail.com>
2023-06-20 17:33:22 +0200

    Vjezbe - Linijska Struktura - Postavljen 17. zadatak

---
+++ Vjezbe/Linijska Struktura/17.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji na osnovu ucitanog cetverocifrenog broja formira i ispisuje novi broj takav da je raspored cifara obrnut
+u odnosu na raspored cifara ucitanog broja (unos 2378, ispis 8732)
+*/
