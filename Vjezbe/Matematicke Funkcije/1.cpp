ad1fce636fd782b462d7efb0af0785b0fbe6e095
Danin Sadzak <daninsadzak@gmail.com>
2023-06-20 19:17:18 +0200

    Vjezbe - Matematicke Funkcije - Zavrsen 1. zadatak

--- Vjezbe/Matematicke Funkcije/1.cpp
+++ Vjezbe/Matematicke Funkcije/1.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-
+    float x1, x2, y1, y2;
+    cout << "Unesite koordinate tacke A!\n";
+    cin >> x1 >> y1;
+    cout << "Unesite koordinate tacke B!\n";
+    cin >> x2 >> y2;
+    cout << "Njihova udaljenost: " << sqrt((pow(x2 - x1, 2) + pow(y2 - y2, 2)));
     return 0;
 }
 /*
