d3d213e64927506ba415419a28bdcf8726a06794
Danin Sadzak <daninsadzak@gmail.com>
2023-06-20 19:02:09 +0200

    Pripreme - Linijska Struktura - Postavljen 8. zadatak

---
+++ Pripreme/Linijska Struktura/8.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji ucitava dva dvocifrena broja i ispisuje sumu desetica te proizvod jedinica ta dva broja
+*/
