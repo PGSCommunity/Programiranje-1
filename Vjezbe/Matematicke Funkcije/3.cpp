b8279655ed643ed8841debf617c9fddc0619fa2c
Danin Sadzak <daninsadzak@gmail.com>
2023-08-18 18:53:52 +0200

    Vjezbe - Matematicke Funkcije - Postavljen 3. zadatak

---
+++ Vjezbe/Matematicke Funkcije/3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji omogućava izračun brzine čamca u odnosu na obalu i ugla pod kojim će čamac skrenuti sa svoje putanje. 
+Za oba izračuna koristite Pitagorinu teoremu i trigonometrijsku funkciju.
+*/
