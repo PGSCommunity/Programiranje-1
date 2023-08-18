0f870c9ab3e465a0b8b68b6b707cd55232fcec02
Danin Sadzak <daninsadzak@gmail.com>
2023-08-18 18:58:45 +0200

    Vjezbe - Matematicke Funkcije - Zavrsen 3. zadatak

--- Vjezbe/Matematicke Funkcije/3.cpp
+++ Vjezbe/Matematicke Funkcije/3.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
+    float a,b;
+    cout << "Unesi brzinu rijeke (m/s):\n";
+    cin >> a;
+    
+    cout << "Unesi brzinu camca (m/s):\n";
+    cin >> b;
 
+    float c = sqrt(pow(a,2) + pow(b,2));
+
+    float ugao = atan(a / b);
+    ugao = ugao * 180 / 3.14;
+
+    cout << "Camac se u odnosu na obalu krece " << c << "m/s\n";
+    cout << "Camac je skrenuo sa svoje putanje za " << ugao << " stepeni.";
     return 0;
 }
 /*
