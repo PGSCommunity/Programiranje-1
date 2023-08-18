61316e51da2dd712bce67c0f4c836d2360aab263
Danin Sadzak <daninsadzak@gmail.com>
2023-08-18 19:09:07 +0200

    Vjezbe - Matematicke Funkcije - Zavrsen 4. zadatak

--- Vjezbe/Matematicke Funkcije/4.cpp
+++ Vjezbe/Matematicke Funkcije/4.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main() {
+    int a;
+    cout << "Unesite broj a!\n";
+    cin >> a;
 
+    cout << "Kvadrat broja: " << pow(a, 2) << '\n';
+    cout << "Korijen broja: " << sqrt(a) << '\n';
     return 0;
 }
 /*
