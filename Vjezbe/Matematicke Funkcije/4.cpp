b51494ce1df2226840e9b2c85d50eb805562ce39
Danin Sadzak <daninsadzak@gmail.com>
2023-08-18 19:00:12 +0200

    Vjezbe - Matematicke Funkcije - Postavljen 4. zadatak

---
+++ Vjezbe/Matematicke Funkcije/4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+U koordinatnom sistemu date su četiri tačke. Napisati program kojim se izračunava obim četverougla čiji su vrhovi date tačke.
+*/
